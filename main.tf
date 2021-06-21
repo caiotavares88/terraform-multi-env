c7f8172e9f252e24f8237af3eefaa7e34723cc21
Caio Tavares <caio.tavares@ditoweb.com>
2021-06-21 11:32:52 -0400

    fixed variables prod

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_pubsub_topic" "topic" {
   name = var.topic_name
 
   labels = {
-        environment        = var.environment		#(dev, staging, production)
+        environment        = "production"		#(dev, staging, production)
         changed_by         = "terraform"				#Person or automation that last changed any configuration
         taskid             = "000000001"				#Zendesk ticket number
   } 
--- production/terraform.tfvars
+++ production/terraform.tfvars
@@ -1,3 +1,4 @@
 project = "kubernetes-project-313715"
 region  = "us-east1"
 zone    = "us-east1-c"
+topic_name = "production_topic"
